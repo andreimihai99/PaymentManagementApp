56e0afa0b476975ad87df17581815a746c05d5dc
Andrei Mihai <andreimihai99@yahoo.com>
2021-03-23 17:43:02 +0200

    Added first implementation of the Home Page

---
+++ src/main/java/HomePageController.java
@@ -0,0 +1,23 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HomePage {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    void changeToLoginPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeToRegisterPage(ActionEvent event) {
+
+    }
+
+}

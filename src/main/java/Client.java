e370853b3a5afbec438e645f5569c784db35e0af
Andrei Mihai <andreimihai99@yahoo.com>
2021-03-26 16:06:32 +0200

    Small steps forward

---
+++ src/main/java/Client.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+
+public class Main extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        String fxmlPath = "src/main/resources/HomePage.fxml";
+
+        FileInputStream fxmlStream = new FileInputStream(fxmlPath);
+        Pane root = (Pane)loader.load(fxmlStream);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+}
